04a5c821a4985475406d5e4db429a40bc629d8cb
Dermot Boyle <dermotboyled@gmail.com>
2022-12-14 00:39:55 +0100

    readme

--- App/App.tsx
+++ App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { NavigationContainer } from '@react-navigation/native'
-import EntryPoint from '../EntryPoint'
+import EntryPoint from './EntryPoint'
 
 const queryClient = new QueryClient()
 
--- App/EntryPoint.tsx
+++ App/EntryPoint.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import HotelSearchPage from './App/domains/HotelSearchPage'
-import { theme } from './App/variables'
+import HotelSearchPage from './domains/HotelSearchPage'
+import { theme } from './variables'
 
 const Stack = createStackNavigator()
 
